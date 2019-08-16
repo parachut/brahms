8f2cd297af23eca7c83c66e52ee6a4f65b7f2fda
Daniel Wallace <daniel@parachut.co>
2019-08-16 14:31:58 -0400

    Update createAuthyUser.ts

--- src/tasks/createAuthyUser.ts
+++ src/tasks/createAuthyUser.ts
@@ -8,30 +8,22 @@ const authy = new Authy({ key: process.env.AUTHY });
 async function createAuthyUser(job) {
   const { userId } = job.data;
 
-  console.log(job.data);
-
   if (userId) {
-    try {
-      const user = await User.findByPk(userId, { include: ['integrations'] });
+    const user = await User.findByPk(userId, { include: ['integrations'] });
 
-      console.log(user);
+    const {
+      user: { id: authyId },
+    } = await authy.registerUser({
+      countryCode: 'US',
+      email: user.email,
+      phone: user.phone,
+    });
 
-      const {
-        user: { id: authyId },
-      } = await authy.registerUser({
-        countryCode: 'US',
-        email: user.email,
-        phone: user.phone,
-      });
-
-      await UserIntegration.create({
-        type: 'AUTHY',
-        value: authyId,
-        userId: user.id,
-      });
-    } catch (e) {
-      console.log(e);
-    }
+    await UserIntegration.create({
+      type: 'AUTHY',
+      value: authyId,
+      userId: user.id,
+    });
 
     return `User authy account created: ${userId} ${authyId}`;
   }
