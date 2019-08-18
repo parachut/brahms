f8c37668153071fb460115dd750b2d6b97490735
Daniel Wallace <daniel@parachut.co>
2019-08-18 17:28:08 -0400

    fix redis

--- src/redis.ts
+++ src/redis.ts
@@ -1,18 +1,9 @@
 import { RedisPubSub } from 'graphql-redis-subscriptions';
 import Redis from 'ioredis';
 
-export const redis = new Redis(
-  parseInt(process.env.REDIS_PORT),
-  process.env.REDIS_HOST,
-);
+export const redis = new Redis(process.env.REDIS_URL);
 
 export const pubSub = new RedisPubSub({
-  publisher: new Redis(
-    parseInt(process.env.REDIS_PORT),
-    process.env.REDIS_HOST,
-  ),
-  subscriber: new Redis(
-    parseInt(process.env.REDIS_PORT),
-    process.env.REDIS_HOST,
-  ),
+  publisher: new Redis(process.env.REDIS_URL),
+  subscriber: new Redis(process.env.REDIS_URL),
 });
