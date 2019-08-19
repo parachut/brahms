c02a0bb56386844cb66c833c48ab822966753b75
Daniel Wallace <daniel@parachut.co>
2019-08-19 15:55:10 -0400

    fix

--- src/forest/cart.ts
+++ src/forest/cart.ts
@@ -8,24 +8,5 @@ Liana.collection('Cart', {
     {
       name: 'Cancel Cart',
     },
-    {
-      name: 'Create Shipment',
-      type: 'single',
-      fields: [
-        {
-          field: 'inventory',
-          description: 'Select the inventory for the shipment.',
-          type: ['String'],
-          isRequired: true,
-        },
-        {
-          field: 'service',
-          description: 'Select a UPS service for the shipment.',
-          type: ['Enum'],
-          enums: ['Ground', '2ndDayAir', 'NextDayAir'],
-          isRequired: true,
-        },
-      ],
-    },
   ],
 });
