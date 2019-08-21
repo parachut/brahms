aa10e36b8dd4e8582f244cc7e52309263a3a99a6
Daniel Wallace <daniel@parachut.co>
2019-08-21 16:55:31 -0600

    Update confirmCart.ts

--- src/routes/confirmCart.ts
+++ src/routes/confirmCart.ts
@@ -3,6 +3,10 @@ import Liana from 'forest-express-sequelize';
 import { Op } from 'sequelize';
 
 import { Cart } from '../models/Cart';
+import { Inventory } from '../models/Inventory';
+import { Shipment } from '../models/Shipment';
+import { ShipmentDirection } from '../enums/shipmentDirection';
+import { ShipmentType } from '../enums/shipmentType';
 import { createQueue } from '../redis';
 
 const communicationQueue = createQueue('communication-queue');
@@ -28,6 +32,22 @@ router.post(
       communicationQueue.add('send-outbound-access-confirmation-email', {
         cartId: id,
       });
+
+      const [inventory, shipment] = await Promise.all([
+        Inventory.findAll({
+          where: {
+            cartId: id,
+          },
+        }),
+        Shipment.create({
+          direction: ShipmentDirection.OUTBOUND,
+          type: ShipmentType.ACCESS,
+          service: '2ndDayAirAM',
+          cartId: id,
+        }),
+      ]);
+
+      await shipment.$set('inventory', inventory);
     }
 
     await res.send({ success: 'Cart(s) are confirmed!' });
