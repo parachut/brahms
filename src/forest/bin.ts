07e3cbab835a3fd7d293a9d8eb311bfc3258a86d
Daniel Wallace <daniel@parachut.co>
2019-09-18 15:54:34 -0600

    Update bin.ts

--- src/forest/bin.ts
+++ src/forest/bin.ts
@@ -1,11 +1,23 @@
 import Liana from 'forest-express-sequelize';
 
+import { Bin } from '../models/Bin';
+
 Liana.collection('Bin', {
   actions: [
     {
       name: 'Generate labels',
       type: 'global',
       download: true,
+    },
+  ],
+  fields: [
+    {
+      field: 'fullName',
+      type: 'String',
+      readonly: true,
+      get: async (bin: Bin) => {
+        return `${bin.location}-${bin.row}-${bin.column}-${bin.cell}`;
+      },
     },
   ],
 });
