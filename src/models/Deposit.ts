accc5234d0d546f8ec78f7f0fc4d1aa5ed52a8b6
Daniel Wallace <daniel@parachut.co>
2019-10-01 13:14:02 -0400

    deposit/income records

---
+++ src/models/Deposit.ts
@@ -0,0 +1,52 @@
+import Sequelize from 'sequelize';
+import {
+  BelongsTo,
+  Column,
+  CreatedAt,
+  DataType,
+  Default,
+  ForeignKey,
+  Model,
+  PrimaryKey,
+  Table,
+  UpdatedAt,
+} from 'sequelize-typescript';
+import { Field, ID, ObjectType } from 'type-graphql';
+
+import { User } from './User';
+
+@ObjectType()
+@Table
+export class Deposit extends Model<Deposit> {
+  @Field((type) => ID)
+  @PrimaryKey
+  @Default(Sequelize.literal('uuid_generate_v4()'))
+  @Column(DataType.UUID)
+  public id!: string;
+
+  @Field()
+  @Default(false)
+  @Column
+  public amount!: boolean;
+
+  @Field({ nullable: true })
+  @Column
+  public notes?: string;
+
+  @Field({ nullable: true })
+  @Column
+  public transferId?: string;
+
+  @BelongsTo(() => User, 'userId')
+  public user!: User;
+
+  @ForeignKey(() => User)
+  @Column(DataType.UUID)
+  public userId!: string;
+
+  @CreatedAt
+  public createdAt!: Date;
+
+  @UpdatedAt
+  public updatedAt!: Date;
+}
