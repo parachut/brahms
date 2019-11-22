9c0fae36d461218582d39cae8c38ec82231656fe
Daniel Wallace <daniel@parachut.co>
2019-11-22 12:21:53 -0500

    database clean up

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,35 @@
 import { Sequelize } from 'sequelize-typescript';
+import parseDbUrl from 'parse-database-url';
 
-export const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  modelPaths: [`${__dirname}/models`],
-  logging: false,
-  dialectOptions: {
-    ssl: true,
+const dbConfig = parseDbUrl(process.env['DATABASE_URL']);
+const dbRead = parseDbUrl(process.env['HEROKU_POSTGRESQL_BRONZE_URL']);
+
+export const sequelize = new Sequelize(
+  dbConfig.database,
+  dbConfig.user,
+  dbConfig.password,
+  {
+    dialect: 'postgres',
+    modelPaths: [`${__dirname}/models`],
+    logging: false,
+    dialectOptions: {
+      ssl: true,
+    },
+    replication: {
+      read: [
+        {
+          host: dbRead.host,
+          port: dbRead.port,
+        },
+      ],
+      write: {
+        host: dbConfig.host,
+        port: dbConfig.port,
+      },
+    },
+    pool: {
+      max: 20,
+      idle: 30000,
+    },
   },
-});
+);
