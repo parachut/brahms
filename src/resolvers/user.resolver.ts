0dcec267c0873ce51b6f6cc20daf7c89556de704
Daniel Wallace <daniel@parachut.co>
2019-12-03 11:50:18 -0500

    Update user.resolver.ts

--- src/resolvers/user.resolver.ts
+++ src/resolvers/user.resolver.ts
@@ -62,11 +62,13 @@ export default class UserResolver {
     const income = ((await user.$get<Income>('incomes')) as Income[])!;
     const deposits = ((await user.$get<Deposit>('deposits')) as Deposit[])!;
 
-    const totalIncome = income.reduce((r, i) => r + i.commission, 0);
-    const totalDeposited = deposits.reduce((r, i) => r + i.amount, 0);
+    const totalIncome =
+      Math.round(income.reduce((r, i) => r + i.commission, 0) * 100) / 100;
+    const totalDeposited =
+      Math.round(deposits.reduce((r, i) => r + i.amount, 0) * 100) / 100;
 
     return {
-      available: totalIncome - totalDeposited,
+      available: Math.round((totalIncome - totalDeposited) * 100) / 100,
       totalIncome,
       totalDeposited,
     };
