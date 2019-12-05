3e0fcd726de66126437e391977166cf0b75dfc24
Kodee <kodee@parachut.co>
2019-12-05 11:11:30 -0500

    added the functionality for additional items

--- src/models/User.ts
+++ src/models/User.ts
@@ -135,6 +135,10 @@ export class User extends Model<User> {
   public stripeId?: string;
 
   @Field()
+  @Column
+  public additionalItems!: number;
+
+  @Field()
   @Default(false)
   @Column
   public protectionPlan!: boolean;
