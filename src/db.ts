7d56b0bef67b8234d59aaded29b16ac2b06338be
Daniel Wallace <daniel@parachut.co>
2019-12-24 11:24:16 -0500

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize-typescript'
 import parseDbUrl from 'parse-database-url'
 
 const dbConfig = parseDbUrl(process.env['DATABASE_URL'])
-const dbRead = parseDbUrl(process.env['HEROKU_POSTGRESQL_COPPER'])
+const dbRead = parseDbUrl(process.env['HEROKU_POSTGRESQL_COPPER_URL'])
 
 export const sequelize = new Sequelize(
   dbConfig.database,
