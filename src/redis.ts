11ee3a9357f07c7a5d8ddf8f13a90f50f098051f
Daniel Wallace <41707979+parachut-daniel@users.noreply.github.com>
2020-02-03 11:04:20 -0500

    remove tasks

--- src/redis.ts
+++
@@ -1,8 +0,0 @@
-import Redis from 'ioredis';
-import Queue from 'bull';
-
-export const redis = new Redis(process.env.REDIS_URL);
-
-export function createQueue(name: string): InstanceType<Queue> {
-  return new Queue(name, process.env.REDIS_URL);
-}
--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,25 @@
 import Analytics from 'analytics-node';
 import { ApolloServer } from 'apollo-server-express';
-import cors from 'cors';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import { createContext } from 'dataloader-sequelize';
 import express from 'express';
 import expressJwt from 'express-jwt';
 import fs from 'fs';
-import jwt from 'jsonwebtoken';
 import requestIp from 'request-ip';
 import { buildSchema } from 'type-graphql';
-// import jsonwebtoken from 'jsonwebtoken';
+
+import { sequelize } from './db';
+import { customAuthChecker } from './utils/customAuthChecker';
 
 require('dotenv').config();
 
-import { signOptions } from '../certs';
-import { sequelize } from './db';
-import hooks from './hooks';
-import { redis } from './redis';
-import { customAuthChecker } from './utils/customAuthChecker';
-
-// import { migrator } from './migrator';
 const PORT = process.env.PORT || 4000;
 const GQLPATH = '/graphql';
 
 const analytics = new Analytics(process.env.SEGMENT);
-const jwtSecret = fs.readFileSync('./certs/private.key', 'utf8');
 
 const main = async () => {
-  /**
-  const token = jsonwebtoken.sign(
-    {
-      id: '760e585b-ae7f-4146-81aa-e783d0b9a218',
-      roles: ['MEMBER'],
-    },
-    fs.readFileSync('./certs/private.key', 'utf8'),
-    signOptions,
-  );
-
-    console.log(token);
-
-
-   */
-
   const dataloaderContext = createContext(sequelize);
 
   const schema = await buildSchema({
@@ -53,7 +31,6 @@ const main = async () => {
   });
 
   const app = express();
-  // app.use('/migrator', migrator);
 
   app.use(GQLPATH, cors());
 
@@ -70,7 +47,6 @@ const main = async () => {
         analytics,
         clientIp: requestIp.getClientIp(req),
         dataloaderContext,
-        redis,
         req,
         sequelize,
         user: req.user,
@@ -91,25 +67,6 @@ const main = async () => {
   );
 
   server.applyMiddleware({ app, path: GQLPATH });
-
-  app.use('/hooks', hooks);
-  // app.use('/cron', cron);
-
-  app.use(
-    require('forest-express-sequelize').init({
-      authSecret: process.env.FOREST_AUTH_SECRET,
-      configDir: __dirname + '/forest',
-      envSecret: process.env.FOREST_ENV_SECRET,
-      modelsDir: __dirname + '/models',
-      sequelize,
-    }),
-  );
-
-  fs.readdirSync(__dirname + '/routes').forEach((file) => {
-    if (file[0] !== '.') {
-      app.use('/forest', require('./routes/' + file));
-    }
-  });
 
   app.listen(PORT, () => {
     // Instantiates a client.
