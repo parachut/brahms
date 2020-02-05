64def0a2a11d9973eaa0de47e76a4872eee86e85
Daniel Wallace <41707979+parachut-daniel@users.noreply.github.com>
2020-02-04 21:32:00 -0500

    .

--- src/db.ts
+++ src/db.ts
@@ -1,35 +1,29 @@
-import { Sequelize } from 'sequelize-typescript'
-import parseDbUrl from 'parse-database-url'
-
-const dbConfig = parseDbUrl(process.env['DATABASE_URL'])
-const dbRead = parseDbUrl(process.env['HEROKU_POSTGRESQL_COPPER_URL'])
+import { Sequelize } from 'sequelize-typescript';
+import { readFileSync } from 'fs';
 
 export const sequelize = new Sequelize(
-  dbConfig.database,
-  dbConfig.user,
-  dbConfig.password,
+  'da4vhsd5as770l',
+  '37CDm24TUX7sKMJWCCBUtxYRr',
+  'CJC6QupaPb3rcR4r8dJpj8W77',
   {
     dialect: 'postgres',
     modelPaths: [`${__dirname}/models`],
     logging: false,
+    host: process.env.POSTGRES_HOST,
     dialectOptions: {
-      ssl: true,
-    },
-    replication: {
-      read: [
-        {
-          host: dbRead.host,
-          port: dbRead.port,
-        },
-      ],
-      write: {
-        host: dbConfig.host,
-        port: dbConfig.port,
-      },
+      ssl:
+        process.env.NODE_ENV !== 'production'
+          ? {
+              ca: readFileSync(__dirname + '/sql/server-ca.pem'),
+              cert: readFileSync(__dirname + '/sql/client.pem'),
+              key: readFileSync(__dirname + '/sql/client.key'),
+              rejectUnauthorized: false,
+            }
+          : undefined,
     },
     pool: {
       max: 20,
       idle: 30000,
     },
   },
-)
+);
